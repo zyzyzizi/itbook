6e395ac3d01f07cef1424f118267f2f3c99a31ad
hokwon <zyzyzizi@gmail.com>
2022-03-26 23:03:54 +0900

    fix tc failed

--- app/src/test/java/com/twobros/itstore/SearchViewModelTest.kt
+++ app/src/test/java/com/twobros/itstore/SearchViewModelTest.kt
@@ -1,20 +1,17 @@
 package com.twobros.itstore
 
-import android.os.Build
+import android.app.Application
 import android.os.Looper
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.twobros.itstore.repostory.BookStoreRepository
-import com.twobros.itstore.repostory.api.BookStoreApiProvider
 import com.twobros.itstore.repostory.api.model.Book
 import com.twobros.itstore.viewmodel.SearchViewModel
-import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
-import org.robolectric.shadow.api.Shadow
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,7 +21,7 @@ import org.robolectric.shadow.api.Shadow
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [28])
 class SearchViewModelTest {
-
+    private val mockApp: Application = mockk(relaxed = true)
 
     private fun makeMockProvider(): BookStoreRepository {
         return mockk(relaxed = true)
@@ -37,25 +34,25 @@ class SearchViewModelTest {
         val test1 = "java"
         val queries1 = Queries(test1)
 
-        val searchViewModel = SearchViewModel(makeMockProvider()).apply {
+        val searchViewModel = SearchViewModel(mockApp, makeMockProvider()).apply {
             init(queries1)
         }
-        val booklist = ArrayList<Book>()
+        val bookList = ArrayList<Book>()
         val books = listOf(makeBookStub("aa"), makeBookStub("bb"), makeBookStub("aa bb"))
-        booklist.addAll(books)
+        bookList.addAll(books)
 
         searchViewModel.updateResultList(books)
 
         shadowOf(Looper.getMainLooper()).idle()
 
         Assert.assertEquals(books, searchViewModel.bookLiveData.value)
-        Assert.assertTrue(booklist.containsAll(searchViewModel.bookList))
+        Assert.assertTrue(bookList.containsAll(searchViewModel.bookList))
 
         val books1 = listOf(makeBookStub("1"), makeBookStub("2"), makeBookStub("3 4"))
-        booklist.addAll(books1)
+        bookList.addAll(books1)
         searchViewModel.updateResultList(books1)
         Assert.assertEquals(books1, searchViewModel.bookLiveData.value)
-        Assert.assertTrue(booklist.containsAll(searchViewModel.bookList))
+        Assert.assertTrue(bookList.containsAll(searchViewModel.bookList))
     }
 
     @Test
@@ -63,7 +60,7 @@ class SearchViewModelTest {
         val test1 = "java-kotlin"
         val queries1 = Queries(test1)
 
-        val searchViewModel = SearchViewModel(makeMockProvider()).apply {
+        val searchViewModel = SearchViewModel(mockApp, makeMockProvider()).apply {
             init(queries1)
         }
 
